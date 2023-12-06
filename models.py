568945dcbc76b8d561c6c023895d4f9a42906891
gkwk <124476558+gkwk@users.noreply.github.com>
2023-12-06 20:20:25 +0900

    [feat] models 추가

---
+++ models.py
@@ -0,0 +1,26 @@
+from sqlalchemy import Column, Integer, String, Text, DateTime, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
+
+from database import Base
+
+class User(Base):
+    __tablename__ = "user"
+
+    id = Column(Integer, primary_key=True)
+    
+    name = Column(String, nullable=False)
+    password = Column(String, nullable=False)
+    join_date = Column(DateTime, nullable=False)
+    is_superuser = Column(Boolean, nullable=False, default=False)
+
+class Todo(Base):
+    __tablename__ = "todo"
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("user.id"))
+    user = relationship("User", backref="Todos")
+    
+    todo_name = Column(Text, nullable=False)
+    text = Column(String, nullable=False, default="")
+    create_date = Column(DateTime, nullable=False)
+    is_finished = Column(Boolean, nullable=False, default=False)
