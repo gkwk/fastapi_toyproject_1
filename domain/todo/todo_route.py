2e0c48f33bef71e80e4c92899424b894c208a880
gkwk <124476558+gkwk@users.noreply.github.com>
2023-12-07 20:19:49 +0900

    [feat] todo 라우터 수정

--- domain/todo/todo_route.py
+++ domain/todo/todo_route.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 
 from database import get_db
 from models import Todo
-from domain.todo import todo_schema
+from domain.todo import todo_schema, todo_crud
 
 router = APIRouter(
     prefix="/api/todo",
@@ -11,5 +11,5 @@ router = APIRouter(
 
 @router.get("/list", response_model=list[todo_schema.Todo])
 def todo_list(db: Session = Depends(get_db)):
-    _todo_list = db.query(Todo).order_by(Todo.create_date.desc()).all()
+    _todo_list = todo_crud.getTodoList(db=db)
     return _todo_list
