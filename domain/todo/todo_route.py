f506909e49a485c46a11cefc152c5170eb5345ee
gkwk <124476558+gkwk@users.noreply.github.com>
2023-12-07 10:40:17 +0900

    [feat] todo 페이지 수정

--- domain/todo/todo_route.py
+++ domain/todo/todo_route.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 
 from database import SessionLocal
 from models import Todo
+from database import get_db
 
 router = APIRouter(
     prefix="/api/todo",
@@ -9,7 +10,6 @@ router = APIRouter(
 
 @router.get("/list")
 def todo_list():
-    db = SessionLocal()
-    _todo_list = db.query(Todo).order_by(Todo.create_date.desc()).all()
-    db.close()
+    with get_db() as db:
+        _todo_list = db.query(Todo).order_by(Todo.create_date.desc()).all()
     return _todo_list
