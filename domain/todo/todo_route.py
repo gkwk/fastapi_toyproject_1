513a15833b72c53ec599cf09d7b4d9d60bafcf8f
gkwk <124476558+gkwk@users.noreply.github.com>
2023-12-07 10:44:12 +0900

    [feat] db 종료를 Depends로 구현

--- domain/todo/todo_route.py
+++ domain/todo/todo_route.py
@@ -1,15 +1,14 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
 
-from database import SessionLocal
-from models import Todo
 from database import get_db
+from models import Todo
 
 router = APIRouter(
     prefix="/api/todo",
 )
 
 @router.get("/list")
-def todo_list():
-    with get_db() as db:
-        _todo_list = db.query(Todo).order_by(Todo.create_date.desc()).all()
+def todo_list(db: Session = Depends(get_db)):
+    _todo_list = db.query(Todo).order_by(Todo.create_date.desc()).all()
     return _todo_list
