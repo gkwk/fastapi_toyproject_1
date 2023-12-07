c17b9e4543201c1450c52145838232495f54403b
gkwk <124476558+gkwk@users.noreply.github.com>
2023-12-07 10:30:45 +0900

    [feat] main 수정

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
 
+from starlette.middleware.cors import CORSMiddleware
+
+from domain.todo import todo_route
+
 # uvicorn main:{변수명} --reload ex) uvicorn main:app --reload
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+origins = [
+    "http://127.0.0.1:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+app.include_router(todo_route.router)
