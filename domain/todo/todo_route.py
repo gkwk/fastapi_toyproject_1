a53d6ead6b26c69536165a3769861224ac965a34
gkwk <124476558+gkwk@users.noreply.github.com>
2023-12-07 20:10:02 +0900

    [feat] todo 스키마 todo 라우터에 추가

--- domain/todo/todo_route.py
+++ domain/todo/todo_route.py
@@ -3,12 +3,13 @@ from sqlalchemy.orm import Session
 
 from database import get_db
 from models import Todo
+from domain.todo import todo_schema
 
 router = APIRouter(
     prefix="/api/todo",
 )
 
-@router.get("/list")
+@router.get("/list", response_model=list[todo_schema.Todo])
 def todo_list(db: Session = Depends(get_db)):
     _todo_list = db.query(Todo).order_by(Todo.create_date.desc()).all()
     return _todo_list
