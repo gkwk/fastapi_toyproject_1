233cf98a3c4774d17fb0047774af2f64c905edd4
gkwk <124476558+gkwk@users.noreply.github.com>
2023-12-09 16:35:50 +0900

    [feat] todo CRUD 추가

--- domain/todo/todo_crud.py
+++ domain/todo/todo_crud.py
@@ -1,6 +1,10 @@
 from models import Todo
 from sqlalchemy.orm import Session
 
-def getTodoList(db: Session):
-    todoList = db.query(Todo).order_by(Todo.create_date.desc()).all()
+def getTodoList(db: Session, userId):
+    todoList = db.query(Todo).filter_by(user_id = userId).order_by(Todo.create_date.desc()).all()
     return todoList
+
+def getTodoDetail(db: Session, userId, todoId):
+    todoDetail = db.query(Todo).filter_by(user_id = userId, id = todoId)
+    return todoDetail
