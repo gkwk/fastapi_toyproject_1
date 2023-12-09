7c33e04461fe0bb7d181c55036ae016e49d471ec
gkwk <124476558+gkwk@users.noreply.github.com>
2023-12-09 16:36:26 +0900

    [feat] todo 관련 api 추가

--- domain/todo/todo_route.py
+++ domain/todo/todo_route.py
@@ -9,7 +9,12 @@ router = APIRouter(
     prefix="/api/todo",
 )
 
-@router.get("/list", response_model=list[todo_schema.Todo])
-def todo_list(db: Session = Depends(get_db)):
-    _todo_list = todo_crud.getTodoList(db=db)
-    return _todo_list
+@router.get("/list/{userId}", response_model=list[todo_schema.TodoList])
+def getTodoList(userId : int, db: Session = Depends(get_db)):
+    todoList = todo_crud.getTodoList(db=db,userId=userId)
+    return todoList
+
+@router.get("/detail/{userId}/{todoId}", response_model=list[todo_schema.TodoDetail])
+def getTodoDetail(userId : int, todoId : int, db: Session = Depends(get_db)):
+    todoDetail = todo_crud.getTodoDetail(db=db,userId=userId,todoId=todoId)
+    return todoDetail
