b32b3a38aefda413400c1c6cbfbe3fa1cb615ed8
gkwk <124476558+gkwk@users.noreply.github.com>
2023-12-11 19:27:41 +0900

    [feat] todo create를 위한 스키마 추가

--- domain/todo/todo_schema.py
+++ domain/todo/todo_schema.py
@@ -1,6 +1,6 @@
 import datetime
 
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
 
 class TodoList(BaseModel):
     id: int
@@ -18,3 +18,23 @@ class TodoDetail(BaseModel):
     text : str
     create_date : datetime.datetime
     is_finished : bool = False
+
+class CreateTodo(BaseModel):
+    user_id : int
+    
+    todo_name : str
+    text : str
+    
+    @field_validator("user_id")
+    def isInteger(cls, value : int):
+        try:
+            int(value)
+        except:
+            raise ValueError("값이 정수여야 합니다.")
+        return int(value)
+    
+    @field_validator("todo_name", "text")
+    def isNotEmpty(cls, value : str):
+        if not value.strip():
+            raise ValueError("값이 공백일 수 없습니다.")
+        return value
