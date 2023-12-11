c198fb1558cdcac80924497cc4f8d9134a226115
gkwk <124476558+gkwk@users.noreply.github.com>
2023-12-11 19:31:27 +0900

    [feat] main에 user 라우터 연결

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
 from domain.todo import todo_route
+from domain.user import user_route
 
 # uvicorn main:{변수명} --reload ex) uvicorn main:app --reload
 app = FastAPI()
@@ -19,5 +20,5 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
+app.include_router(user_route.router)
 app.include_router(todo_route.router)
