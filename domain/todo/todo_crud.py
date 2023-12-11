5d693ebbc3b88af26eca017b2d5abd17e6c474f3
gkwk <124476558+gkwk@users.noreply.github.com>
2023-12-11 19:28:13 +0900

    [feat] todo create 추가

--- domain/todo/todo_crud.py
+++ domain/todo/todo_crud.py
@@ -1,10 +1,19 @@
-from models import Todo
 from sqlalchemy.orm import Session
+from datetime import datetime
+
+from models import Todo
+from domain.todo.todo_schema import CreateTodo
+
 
 def getTodoList(db: Session, userId):
     todoList = db.query(Todo).filter_by(user_id = userId).order_by(Todo.create_date.desc()).all()
     return todoList
 
 def getTodoDetail(db: Session, userId, todoId):
-    todoDetail = db.query(Todo).filter_by(user_id = userId, id = todoId)
+    todoDetail = db.query(Todo).filter_by(user_id = userId, id = todoId).all()
     return todoDetail
+
+def createTodo(db: Session, schema :CreateTodo):
+    todo = Todo(user_id = schema.user_id, todo_name = schema.todo_name, text = schema.text, create_date = datetime.now())
+    db.add(todo)
+    db.commit()
