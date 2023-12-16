3c6af2cb5b79076a6f80cf5e331a6c76afbc0563
gkwk <124476558+gkwk@users.noreply.github.com>
2023-12-16 12:51:48 +0900

    [feat] config 생성

---
+++ config.py
@@ -0,0 +1,13 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+from functools import lru_cache
+
+
+class Settings(BaseSettings):
+    APP_JWT_SECRET_KEY : str
+    
+    model_config = SettingsConfigDict(env_file=".env")
+    
+@lru_cache
+def getSettings():
+    return Settings()
