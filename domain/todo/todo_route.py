b7943d712f4aaeb73898ddfe2369fd46e8c39b2b
gkwk <124476558+gkwk@users.noreply.github.com>
2023-12-18 01:51:04 +0900

    [feat] todo update 라우팅

--- domain/todo/todo_route.py
+++ domain/todo/todo_route.py
@@ -7,7 +7,7 @@ from jose import jwt, JWTError
 from database import get_db
 from models import Todo, User
 from domain.todo import todo_schema, todo_crud
-from domain.todo.todo_schema import CreateTodo
+from domain.todo.todo_schema import CreateTodo, UpdateTodo
 from config import getSettings
 
 router = APIRouter(
@@ -83,3 +83,24 @@ def createTodo(schema :CreateTodo, token = Depends(oauth2Scheme),  db: Session =
             todo_crud.createTodo(db=db, schema=schema, userId=id)
         else:
             raise HTTPException(status_code=404, detail="유저가 존재하지 않습니다.")
+        
+@router.put("/update", status_code=status.HTTP_204_NO_CONTENT)
+def updateTodo(schema: UpdateTodo, token = Depends(oauth2Scheme), db: Session = Depends(get_db)):
+    credentialsException = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="토큰이 유효하지 않습니다.",
+        headers={"WWW-Authenticate": "Bearer"}
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        name: str = payload.get("name")
+        id: int = payload.get("userId")
+        if (name is None) or (id is None):
+            raise credentialsException
+    except JWTError:
+        raise credentialsException
+    else:
+        todoDetail = todo_crud.getTodoDetail(db=db,userId=id,todoId=schema.todoId)
+        if todoDetail is None:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="데이터를 찾을수 없습니다.")
+        todo_crud.updateTodo(db=db,todo=todoDetail,schema=schema)
