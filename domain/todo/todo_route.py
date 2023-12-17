6491169fbd562d96436fde618ea3b26305e8ec1b
gkwk <124476558+gkwk@users.noreply.github.com>
2023-12-17 18:57:24 +0900

    [feat] todo 리스트 라우팅 수정

--- domain/todo/todo_route.py
+++ domain/todo/todo_route.py
@@ -1,20 +1,44 @@
 from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
 from starlette import status
+from jose import jwt, JWTError
 
 from database import get_db
 from models import Todo, User
 from domain.todo import todo_schema, todo_crud
 from domain.todo.todo_schema import CreateTodo
+from config import getSettings
 
 router = APIRouter(
     prefix="/api/todo",
 )
 
-# @router.get("/list/{userId}", response_model=dict[str,int|list[todo_schema.TodoList]]) 
-@router.get("/list/{userId}", response_model=todo_schema.TotalTodoList)
-def getTodoList(userId : int, db: Session = Depends(get_db), page: int = 0, size: int = 10):
-    total, todoList = todo_crud.getTodoList(db=db,userId=userId,skip=page*size, limit=size)
+SECRET_KEY = getSettings().APP_JWT_SECRET_KEY
+ALGORITHM = "HS256"
+oauth2Scheme = OAuth2PasswordBearer(tokenUrl="/api/user/login")
+
+# @router.post("/list", response_model=dict[str,int|list[todo_schema.TodoList]]) 
+@router.post("/list", response_model=todo_schema.TotalTodoList)
+def getTodoList(token = Depends(oauth2Scheme), db: Session = Depends(get_db), page: int = 0, size: int = 10):
+    credentialsException = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="토큰이 유효하지 않습니다.",
+        headers={"WWW-Authenticate": "Bearer"}
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        name: str = payload.get("name")
+        id: int = payload.get("userId")
+        if (name is None) or (id is None):
+            raise credentialsException
+    except JWTError:
+        raise credentialsException
+    else:
+        total, todoList = todo_crud.getTodoList(db=db,userId=id,skip=page*size, limit=size)
+        if todoList is None:
+            raise credentialsException
+    
     return {"total" : total, "todoList" : todoList}
 
 @router.get("/detail/{userId}/{todoId}", response_model=list[todo_schema.TodoDetail])
