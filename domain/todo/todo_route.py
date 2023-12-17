49d461acc2db0e348927e4c4bbf1777034278324
gkwk <124476558+gkwk@users.noreply.github.com>
2023-12-17 19:30:45 +0900

    [feat] todo 상세 정보, create 라우팅 수정

--- domain/todo/todo_route.py
+++ domain/todo/todo_route.py
@@ -19,7 +19,7 @@ ALGORITHM = "HS256"
 oauth2Scheme = OAuth2PasswordBearer(tokenUrl="/api/user/login")
 
 # @router.post("/list", response_model=dict[str,int|list[todo_schema.TodoList]]) 
-@router.post("/list", response_model=todo_schema.TotalTodoList)
+@router.get("/list", response_model=todo_schema.TotalTodoList)
 def getTodoList(token = Depends(oauth2Scheme), db: Session = Depends(get_db), page: int = 0, size: int = 10):
     credentialsException = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
@@ -41,15 +41,45 @@ def getTodoList(token = Depends(oauth2Scheme), db: Session = Depends(get_db), pa
     
     return {"total" : total, "todoList" : todoList}
 
-@router.get("/detail/{userId}/{todoId}", response_model=list[todo_schema.TodoDetail])
-def getTodoDetail(userId : int, todoId : int, db: Session = Depends(get_db)):
-    todoDetail = todo_crud.getTodoDetail(db=db,userId=userId,todoId=todoId)
-    return todoDetail
-
-@router.post("/create/{userId}", status_code=status.HTTP_204_NO_CONTENT)
-def createTodo(schema :CreateTodo ,db: Session = Depends(get_db)):
-    if  db.query(User).filter_by(id = schema.user_id).count() == 1:
-        # userid 검증 방법 개선 필요
-        todo_crud.createTodo(db=db, schema=schema)
+@router.get("/detail/{todoId}", response_model=todo_schema.TodoDetail)
+def getTodoDetail1(todoId : int, token = Depends(oauth2Scheme), db: Session = Depends(get_db)):
+    credentialsException = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="토큰이 유효하지 않습니다.",
+        headers={"WWW-Authenticate": "Bearer"}
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        name: str = payload.get("name")
+        id: int = payload.get("userId")
+        if (name is None) or (id is None):
+            raise credentialsException
+    except JWTError:
+        raise credentialsException
     else:
-        raise HTTPException(status_code=404, detail="User not found")
+        todoDetail = todo_crud.getTodoDetail(db=db,userId=id,todoId=todoId)
+        if todoDetail is None:
+            raise credentialsException
+    
+    return todoDetail
+    
+@router.post("/create", status_code=status.HTTP_204_NO_CONTENT)
+def createTodo(schema :CreateTodo, token = Depends(oauth2Scheme),  db: Session = Depends(get_db)):
+    credentialsException = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="토큰이 유효하지 않습니다.",
+        headers={"WWW-Authenticate": "Bearer"}
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        name: str = payload.get("name")
+        id: int = payload.get("userId")
+        if (name is None) or (id is None):
+            raise credentialsException
+    except JWTError:
+        raise credentialsException
+    else:
+        if db.query(User).filter_by(id = id).count() == 1:
+            todo_crud.createTodo(db=db, schema=schema, userId=id)
+        else:
+            raise HTTPException(status_code=404, detail="유저가 존재하지 않습니다.")
