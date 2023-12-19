4795642b76d299dd0ccbc803f367f94db350761d
gkwk <124476558+gkwk@users.noreply.github.com>
2023-12-19 14:38:42 +0900

    [feat] sqlalchemy db url config로 이동

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ from functools import lru_cache
 
 class Settings(BaseSettings):
     APP_JWT_SECRET_KEY: str
+    SQLALCHEMY_DATABASE_URL: str
 
     model_config = SettingsConfigDict(env_file=".env")
 
