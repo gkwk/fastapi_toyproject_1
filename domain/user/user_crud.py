0103fea590cde71274af872dfc29654e75f409e4
gkwk <124476558+gkwk@users.noreply.github.com>
2023-12-19 16:03:21 +0900

    [style] 포맷팅

--- domain/user/user_crud.py
+++ domain/user/user_crud.py
@@ -1,11 +1,10 @@
-from sqlalchemy.orm import Session
 from datetime import datetime
-from passlib.context import CryptContext
+
+from sqlalchemy.orm import Session
 
 from models import User
-from domain.user.user_schema import UserDetail, UserCreate
-
-password_context = CryptContext(schemes=["bcrypt"])
+from domain.user.user_schema import UserCreate
+from component.auth import get_password_context
 
 
 def does_user_already_exist(data_base: Session, user_create: UserCreate):
@@ -19,7 +18,7 @@ def does_user_already_exist(data_base: Session, user_create: UserCreate):
 def create_user(data_base: Session, user_create: UserCreate):
     user = User(
         name=user_create.name,
-        password=password_context.hash(user_create.password1),
+        password=get_password_context().hash(user_create.password1),
         join_date=datetime.now(),
         email=user_create.email,
     )
@@ -27,11 +26,11 @@ def create_user(data_base: Session, user_create: UserCreate):
     data_base.commit()
 
 
-def get_user(data_base: Session, name: str):
-    user = data_base.query(User).filter(User.name == name).first()
+def get_user(data_base: Session, user_name: str):
+    user = data_base.query(User).filter(User.name == user_name).first()
     return user
 
 
-def get_user_detail(data_base: Session, userId, userName):
-    user_detail = data_base.query(User).filter_by(id=userId, name=userName).first()
+def get_user_detail(data_base: Session, user_id, user_name):
+    user_detail = data_base.query(User).filter_by(id=user_id, name=user_name).first()
     return user_detail
